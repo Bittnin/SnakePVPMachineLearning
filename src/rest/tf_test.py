c3b74a831830a634d9b5196e82dd2911d4238a06
bittnin-Arch <bittninyt@gmail.com>
2023-09-01 23:44:55 +0200

    Linear Regression
    The first algorithm from the tutorial finished.
    The AI has about 76% accuracy

--- src/rest/tf_test.py
+++ src/rest/tf_test.py
@@ -6,7 +6,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from six.moves import urllib
 
-
 # Load dataset
 dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')  # training data
 dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')  # testing data
@@ -24,4 +23,25 @@ for feature_name in CATEGORICAL_COLUMNS:
 for feature_name in NUMERIC_COLUMNS:
     feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
-print(feature_columns)
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function():
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+        if shuffle:
+            ds = ds.shuffle(1000)
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds
+
+    return input_function
+
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+linear_est.train(train_input_fn)
+result = linear_est.evaluate(eval_input_fn)
+
+print(f"Lessssgoooooo einfach eine AI. Trommelwirbel bitte, die Genauigkeit ist {result['accuracy']}")
